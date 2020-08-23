066cc95f717757869aaf278396411fc496a25cce
Victor Shao <vshao98@gmail.com>
2020-08-23 14:59:33 -0400

    Fix chat wheel sounds

--- src/bot.js
+++ src/bot.js
@@ -1,52 +1,67 @@
-const FS_PREFIX = 'src/';
-const REQUIRE_PREFIX = './';
 const Discord = require('discord.js');
 const fs = require('fs');
-const auth = require(REQUIRE_PREFIX + 'auth.json');
+const auth = require('./auth.json');
+
+const FS_PREFIX = 'src/';
+const SOUND_PREFIX = FS_PREFIX + 'sounds/';
 
 const client = new Discord.Client();
 let helpText = "";
 
+// TODO: append sounds to commandList
+// TODO: have !help check sounds/ instead of using help.txt
+// TODO: queue chatwheel sounds
+// TODO: emoji list command
 function handleMessage(message) {
   if (message.content.charAt(0) !== '!') {
     return;
   }
-
-  const command = message.content.substring(1);
-  // const commandArgs = command.split(' ');
   const commandList = {
-    help: () => { help(message); },
-    ping: () => { ping(message); },
+    help: () => { help(message.channel); },
+    ping: () => { ping(message.channel); },
   };
-
+  const command = message.content.substring(1);
   if (commandList.hasOwnProperty(command)) {
     commandList[command]();
+  } else if (fs.existsSync(`${SOUND_PREFIX}${command}.mp3`)) {
+    chatwheel(message, `${SOUND_PREFIX}${command}.mp3`);
   } else {
     message.channel.send('Sorry, I don\'t recognize that command. Type !help for a list.');
   }
 }
 
-function help(message) {
+function chatwheel(message, filename) {
+  const voiceChannel = message.member.voice.channel;
+  if (voiceChannel) {
+    voiceChannel.join().then(connection => {
+      connection.play(filename);
+    });
+  } else {
+    message.channel.send('I can\'t play sounds if you aren\'t in a voice channel.');
+  }
+}
+
+function help(channel) {
   if (helpText) {
-    message.channel.send('Available sounds: ');
-    message.channel.send('`' + helpText + '`');
+    channel.send('Available sounds: ');
+    channel.send('`' + helpText + '`');
   } else {
     fs.readFile(FS_PREFIX + 'help.txt', 'utf8', (error, data) => {
       if (error) {
-        message.channel.send('I\'m not feeling so well. Ask my owner for help.');
+        channel.send('I\'m not feeling so well. Ask my owner for help.');
         console.log(error.name + ': ' + error.message);
       } else {
         helpText = data;
         // Async
-        message.channel.send('Available sounds: ');
-        message.channel.send('`' + helpText + '`');
+        channel.send('Available sounds: ');
+        channel.send('`' + helpText + '`');
       }
     });
   }
 }
 
-function ping(message) {
-  message.channel.send('Pong!');
+function ping(channel) {
+  channel.send('Pong!');
 }
 
 function main() {
